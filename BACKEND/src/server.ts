29e1b4339d881d9c346eaf5f531e4402cc4c98fd
vinke <vinke@tiger.tiger>
2026-05-12 07:02:36 -0500

    actualización del backend y frontend

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -6,6 +6,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "API funcionando 🚀"
+  });
+});
 
 app.get("/usuarios", (req, res) => {
   conexion.query("SELECT * FROM usuario", (err, results) => {
